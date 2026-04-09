ad07dcdff3b5e7d032efdbe9c1e7f1cb05e2f7d3
DmytroLomako <lomakodmytro@gmail.com>
2026-04-09 18:19:50 +0300

    created character and event classes

---
+++ week-10/Character.cs
@@ -0,0 +1,16 @@
+namespace week_10;
+
+public class Character
+{
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public int Level { get; set; }
+    public int Health { get; set; }
+    public int Gold { get; set; }
+    public string State { get; set; }
+
+    public Character(string name, string role, int level, int health, int gold, string state)
+    {
+        Name = name; Role = role; Level = level; Health = health; Gold = gold; State = state;
+    }
+}
---
+++ week-10/Event.cs
@@ -0,0 +1,14 @@
+namespace week_10;
+
+public class Event
+{
+    public int Turn { get; set; }
+    public string Description { get; set; }
+    public string EventType { get; set; } 
+    public string CharChange { get; set; }
+
+    public Event(int turn, string description, string eventType, string charChange)
+    {
+        Turn = turn; Description = description; EventType = eventType; CharChange = charChange;
+    }
+}
